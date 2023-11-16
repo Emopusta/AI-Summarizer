777cbbeddd884b55f85de82b2696db4b23096859
Emopusta <emreduman225@hotmail.com>
2023-11-16 18:24:49 +0300

    added switch with its logic

---
+++ src/components/Switch.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Demo from "./Demo";
+import Memo from "./Memo";
+const Switch = () => {
+  const [isSummarizerLink, setIsSummarizerLink] = useState(true);
+
+  const handleClick = () => {
+    setIsSummarizerLink((prev) => !prev);
+  };
+
+  const buttonClasses = `
+    px-6 py-3 text-white font-semibold rounded-full focus:outline-none mt-10
+    ${
+      isSummarizerLink
+        ? "transition-transform transform -translate-x-full bg-red-500"
+        : "transition-transform transform translate-x-full bg-blue-500"
+    }
+  `;
+
+  return (
+    <div className="flex flex-col items-center">
+      <button className={buttonClasses} onClick={handleClick}>
+        {isSummarizerLink ? "Click for Summarize Text" : "Click for Summarize Link"}
+      </button>
+      {isSummarizerLink ? <Demo /> : <Memo/>}
+    </div>
+  );
+};
+
+export default Switch;
