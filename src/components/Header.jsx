93c147c136837a0cac2b36dc1af6eed1a9f02884
Emopusta <emreduman225@hotmail.com>
2023-11-17 16:37:15 +0300

    changed hero to header and refactored logo and stuff

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import { logo } from "../assets";
-function Hero() {
+function Header() {
   return (
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center flex-row w-full mb-10 pt-3">
+        <div className="flex flex-row items-center">
         <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+        <p className="font-satoshi text-3xl">SumEmopusta</p>
+        </div>
         <button
           type="button"
           onClick={() => window.open("https://github.com/Emopusta")}
@@ -14,13 +17,15 @@ function Hero() {
       </nav>
 
       <h1 className="head_text">
-        Summarize Articles with <br className="max-md:hidden" />{" "}
         <span className="orange_gradient">OpenAI GPT-4</span>
+        <br className="max-md:hidden" />{" "}
+       
+        Kullanarak herhangi bir şeyin özetini çıkar!
       </h1>
-      <h2 className="desc">Simplify your reading with Summize, an open-source article summarizer
-        that transforms lengthy articles into clear and concise summaries </h2>
+      <h2 className="desc">
+        İngilizce, Almanca, ... fark etmeden gpt-4 ile desteklenen herhangi bir dil ile verdiğiniz girdiler sonucu Türkçe çıktı alın.</h2>
     </header>
   );
 }
 
-export default Hero;
+export default Header;
