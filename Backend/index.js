cd25fd09517f9d71f50e76343fb149f83c0b015a
Emopusta <emreduman225@hotmail.com>
2023-11-21 19:31:49 +0300

    added auth

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectMongoDb from "./config/mongoDb.js";
+import authRoutes from "./routes/authRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 const port = process.env.PORT || 8000;
@@ -15,9 +18,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Welcome to Summarizer API...");
 });
+
+app.use("/api/auth", authRoutes)
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
