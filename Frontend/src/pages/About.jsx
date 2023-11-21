39c93c328b01bad4562a6188f73d051f33ed1662
Emopusta <emreduman225@hotmail.com>
2023-11-21 20:30:09 +0300

    added about page

---
+++ Frontend/src/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="py-20 px-16 max-w-6xl mx-auto bg-gray-100">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800 text-center">
+        Hakkımızda
+      </h1>
+      <h3 className="text-2xl font-bold mb-4 text-slate-500">
+        Hoş Geldiniz, herhangi bir makale veya yazıyı artık türkçe özete çevirmek çok kolay.
+      </h3>
+      
+    </div>
+  );
+};
+
+export default About;
