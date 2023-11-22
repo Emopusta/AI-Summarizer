199fd5e733d97740871f3b9cb54fd0dd5fde9dfd
Emopusta <emreduman225@hotmail.com>
2023-11-22 11:37:18 +0300

    added authorization

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { register, login, logout } from "../controllers/authController.js";
+import { verifyUser } from "../middleware/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/", register);
 router.post("/login", login);
-router.post("/logout", logout);
+router.post("/logout", verifyUser, logout);
 
 export default router;
