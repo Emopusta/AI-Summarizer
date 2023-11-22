15afc7f3c6d518fa365f4843cc2588e5e627089e
Emopusta <emreduman225@hotmail.com>
2023-11-22 13:28:39 +0300

    bugFix added apiSlice to store

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -4,7 +4,6 @@ import { logo } from "../assets";
 
 function Header() {
   const { aisum_userInfo } = useSelector((state) => state.auth);
-  console.log(aisum_userInfo);
   return (
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center flex-row w-full mb-10 pt-3">
--- Frontend/src/services/store.js
+++ Frontend/src/services/store.js
@@ -1,24 +1,29 @@
-import { combineReducers, configureStore} from '@reduxjs/toolkit'
-import authReducer from './slices/authSlice';
-import {persistReducer, persistStore} from 'redux-persist';
-import storage from 'redux-persist/lib/storage'
-import { articleApi } from './article'
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import authReducer from "./slices/authSlice";
+import { apiSlice } from "./slices/apiSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { articleApi } from "./article";
 
-const rootReducer = combineReducers({auth: authReducer, [articleApi.reducerPath]:articleApi.reducer})
+const rootReducer = combineReducers({
+  auth: authReducer,
+  [apiSlice.reducerPath]: apiSlice.reducer,
+  [articleApi.reducerPath]: articleApi.reducer
+});
 
 const persistConfig = {
-    key: 'root',
-    storage,
-    version: 1,
-}
+  key: "root",
+  storage,
+  version: 1,
+};
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
-
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const store = configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-      serializableCheck:false,
-    }).concat(articleApi.middleware),
-  })
-  
-  export const persistor = persistStore(store);
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(articleApi.middleware, apiSlice.middleware),
+});
+
+export const persistor = persistStore(store);
