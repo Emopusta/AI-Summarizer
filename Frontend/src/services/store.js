28b402b1fe919e7e601a6b2d982c56cfb3218ea7
Emopusta <emreduman225@hotmail.com>
2023-11-25 11:50:32 +0300

    changed persisted reducers

--- Frontend/src/services/store.js
+++ Frontend/src/services/store.js
@@ -5,19 +5,27 @@ import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { articleApi } from "./article";
 
-const rootReducer = combineReducers({
-  auth: authReducer,
+const nonPersistedReducers = {
   [apiSlice.reducerPath]: apiSlice.reducer,
-  [articleApi.reducerPath]: articleApi.reducer
+  [articleApi.reducerPath]: articleApi.reducer,
+};
+
+const nonPersistedReducer = combineReducers(nonPersistedReducers);
+
+const rootReducer = combineReducers({
+  nonPersisted: nonPersistedReducer,
+  auth: authReducer,
 });
 
 const persistConfig = {
   key: "root",
   storage,
   version: 1,
+  whitelist: ["auth"], 
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
